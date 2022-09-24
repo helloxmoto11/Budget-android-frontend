284981b1bef69a744c466099e56860c31cbf06ec
MannyDesktop <rhcplive11@gmail.com>
2022-09-24 10:07:24 -0500

    Added More Modules.

--- app/src/main/java/com/mypilog/budget/ui/screen/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/mypilog/budget/ui/screen/home/HomeScreenViewModel.kt
@@ -1,12 +1,13 @@
 package com.mypilog.budget.ui.screen.home
 
 import androidx.lifecycle.ViewModel
+import com.mypilog.domain.usecase.expense.GetExpensesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeScreenViewModel @Inject constructor(
-
+    private val getExpensesUseCase: GetExpensesUseCase
 ): ViewModel() {
 
 
