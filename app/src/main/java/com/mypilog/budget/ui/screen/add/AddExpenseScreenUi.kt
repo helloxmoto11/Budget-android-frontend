e760135fdfad672cfca6eb32b4181c6b31d038ba
Manny Caravantes <rhcplive11@gmail.com>
2022-10-04 20:21:20 -0500

    refactor

--- app/src/main/java/com/mypilog/budget/ui/screen/add/AddExpenseScreenUi.kt
+++ app/src/main/java/com/mypilog/budget/ui/screen/add/AddExpenseScreenUi.kt
@@ -1,13 +1,14 @@
 package com.mypilog.budget.ui.screen.add
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,24 +18,69 @@ import com.mypilog.budget.ui.theme.BudgetTheme
 
 @Composable
 fun AddExpenseScreenUi(
-    screenType: ScreenType = ScreenType.PhonePortrait,
-    screenState: AddScreenState
+    screenType: ScreenType = ScreenType.PhonePortrait, screenState: AddScreenState
 ) {
+
+    val modifier = if (screenType is ScreenType.PhonePortrait) {
+        Modifier.fillMaxWidth()
+    } else Modifier.width(480.dp)
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(8.dp)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "EXPENSE")
+
+        BudgetDateInputTextField(
+            modifier = modifier,
+            textFieldValue = TextFieldValue(),
+            onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
+
+        BudgetTextField(
+            modifier = modifier,
+            type = BudgetType.Expenses,
+            textFieldValue = TextFieldValue(),
+            onValueChange = {},
+            placeholder = "Name"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+
+        BudgetTextField(
+            modifier = modifier,
+            type = BudgetType.Expenses,
+            textFieldValue = TextFieldValue(),
+            onValueChange = {},
+            placeholder = "Category"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        BudgetTextField(
+            modifier = modifier,
+            type = BudgetType.Expenses,
+            textFieldValue = TextFieldValue(),
+            onValueChange = {},
+            placeholder = "Cost"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(
+            onClick = { /*TODO*/ }, modifier = modifier
+        ) {
+            Text(text = "Add")
+        }
 
     }
 }
 
 
 @Preview(
-    name = "Foldable",
-    device = Devices.FOLDABLE,
-    uiMode = UI_MODE_NIGHT_YES
+    name = "Foldable", device = Devices.FOLDABLE, uiMode = UI_MODE_NIGHT_YES
 )
 @Preview(
     name = "phone-landscape",
@@ -44,8 +90,7 @@ fun AddExpenseScreenUi(
     uiMode = UI_MODE_NIGHT_YES
 )
 @Preview(
-    name = "phone-portrait",
-    uiMode = UI_MODE_NIGHT_YES
+    name = "phone-portrait", uiMode = UI_MODE_NIGHT_YES
 )
 annotation class MultiScalePreviews
 
@@ -54,7 +99,9 @@ annotation class MultiScalePreviews
 fun PreviewAddExpenseScreenUi() {
     BudgetTheme {
         Surface {
-            AddExpenseScreenUi(screenState = AddScreenState(BudgetType.Expenses))
+            AddExpenseScreenUi(
+                screenType = ScreenType.Foldable, screenState = AddScreenState(BudgetType.Expenses)
+            )
         }
     }
 }
---
+++ app/src/main/java/com/mypilog/budget/ui/screen/add/AddScreenComposables.kt
@@ -0,0 +1,74 @@
+package com.mypilog.budget.ui.screen.add
+
+import android.app.DatePickerDialog
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import com.mypilog.budget.ui.screen.home.BudgetType
+
+@Composable
+fun BudgetTextField(
+    modifier: Modifier,
+    type: BudgetType,
+    textFieldValue: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    placeholder: String
+) {
+
+    when (type) {
+        BudgetType.Assets -> {
+
+        }
+        BudgetType.Expenses -> {
+
+        }
+        BudgetType.Income -> {
+
+        }
+        BudgetType.Liabilities -> {
+
+        }
+    }
+
+    TextField(
+        value = textFieldValue,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        placeholder = { Text(text = placeholder) }
+    )
+}
+
+@Composable
+fun BudgetDateInputTextField(
+    modifier: Modifier,
+    textFieldValue: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit
+) {
+    val context = LocalContext.current
+
+    val datePickerDialog = DatePickerDialog(context)
+
+    datePickerDialog.setOnDateSetListener { datePicker, year, month, day ->
+        onValueChange(TextFieldValue("$month-$day-$year"))
+    }
+
+    TextField(
+        value = textFieldValue,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        placeholder = { Text("Date") },
+        leadingIcon = {
+            IconButton(onClick = { datePickerDialog.show() }) {
+                Icon(imageVector = Icons.Filled.CalendarMonth, contentDescription = "Date")
+            }
+        },
+        singleLine = true
+    )
+}
